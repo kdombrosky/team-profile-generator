9150857c985e4f54c03c958636a411a9f447011b
Kayla Dombrosky <kdombroskyy@live.com>
2021-06-29 21:37:08 -0400

    file setup 2

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+
+// function init() { inquirer.prompt}
+
+
+
+
+
+
+// initialize app
+// init();
